d9aad0ca557eaafabd9eda7a3bee4311b77bdefc
Tyler Evans <tyler.a.p.evans@gmail.com>
2021-09-27 11:16:52 -0600

    Testing VSCode

---
+++ TestCode/HelloWorld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // This is a comment
+    int age;
+    cout << "Hello World" << endl;
+    cout << "What is your age?\n";
+    cin >> age;
+    cout << "In 10 years you are going to be " << age+10;
+}
