bae6790a0d0d2a1ba295df8a5b1a24a7f2e6923b
Tyler <47286889+tapevans@users.noreply.github.com>
2021-10-13 12:26:57 -0600

    Testing of matrix operations

--- Modified Damped Newtons Method Temp/TestEigenLibrary/main.cpp
+++ Modified Damped Newtons Method Temp/TestEigenLibrary/main.cpp
@@ -35,8 +35,27 @@ int main ()
 
     cout << "\nm * v =" << endl << m * v << endl; // The answer I get from Matlab is [1.5 ; 8.25]
 
-    // Want to figure out how to pull out or push in an entire column or vector
+    //std::cout << "\nNow I am going to reshape m to a 1x4 :" << m.resize(1,4) <<  std::endl;
+
+    std::cout << "\nTrying to access only a column or only a row\n";
+    std::cout << "\n The first  column of m is :\n" << m.block(0,0,2,1) << std::endl;
+    std::cout << "\n The second column of m is :\n" << m.block(0,1,2,1) << std::endl;
+    std::cout << "\n The first  row    of m is :\n" << m.block(0,0,1,2) << std::endl;
+    std::cout << "\n The second row    of m is :\n" << m.block(1,0,1,2) << std::endl;
+
+    std::cout << "\nTrying to access only a column or only a row using a different method\n";
+    std::cout << "\n The first  column of m is :\n" << m.col(0) << std::endl;
+    std::cout << "\n The second column of m is :\n" << m.col(1) << std::endl;
+    std::cout << "\n The first  row    of m is :\n" << m.row(0) << std::endl;
+    std::cout << "\n The second row    of m is :\n" << m.row(1) << std::endl;
+
+    std::cout << "\nTrying to add row vectors\n";
+    std::cout << "\n The first  row plus the second row of m is :\n" << m.row(0)+m.row(1) << std::endl;
+
+    // Want to figure out how to pull out or push in an entire column or vector https://eigen.tuxfamily.org/dox/group__TutorialBlockOperations.html 
     // Want to check the operation of adding and subtracting
+    // There is a reshape function but I need to figure out which types of matrix it works for https://eigen.tuxfamily.org/dox/group__TutorialReshape.html (Might not be in the version I am using)
+
 
     return 0;
 }
